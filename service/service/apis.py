a1c3e0122063c8179df03fb2cb06b9759101dc05
Ethan Chen <gradschool.hchen13@gmail.com>
2018-01-14 13:41:26 +0800

    update debug info

--- service/service/apis.py
+++ service/service/apis.py
@@ -29,16 +29,18 @@ def colorize(request):
     serializer = ColorizeSerializer(data=request.data)
     if serializer.is_valid():
         grayscale_raw = serializer.validated_data.get('image')
-        print("image passed: ", grayscale_raw)
         grayscale = load_image(grayscale_raw)
-        print("convert to array, shape:", grayscale.shape)
         rgb = resnet.gen(grayscale)
-        print("RGB generation, shape:", rgb.shape)
         image = array2image(rgb)
         buffer = io.BytesIO()
         image.save(buffer, format='jpeg')
         b64str = base64.b64encode(buffer.getvalue())
-        print("base 64 string length: ", len(b64str))
+
+        print("\nimage passed: ", grayscale_raw, flush=True)
+        print("convert to array, shape:", grayscale.shape, flush=True)
+        print("RGB generation, shape:", rgb.shape, flush=True)
+        print("base 64 string length: ", len(b64str), flush=True, end='\n\n')
+
         return Response(b64str)
 
     return Response(serializer.errors)
